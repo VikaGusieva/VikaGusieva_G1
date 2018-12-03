0d8da6c9c359ca75811462fcc7ef22c513de0b74
Victoria <vm.gusieva@gmail.com>
2018-12-03 20:42:00 +0200

    fisrt commit

---
+++ VikaGusieva/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VikaGusieva")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VikaGusieva")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("afcde51f-bfd3-4f43-bea8-add98a8a2f58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VikaGusieva/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace VikaGusieva
+{
+    public class Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
